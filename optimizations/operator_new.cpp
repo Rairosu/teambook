695a80f126dab2169222fdf7c3e2b0525cb0d626
Nikita Mikhaylov <nikitamikhaylov@MacBook-Pro-Nikita.local>
2017-09-12 12:58:28 +0300

    started

---
+++ optimizations/operator_new.cpp
@@ -0,0 +1,10 @@
+const int SZ = 5e7;
+char buf[SZ];
+int pos;
+void * operator new (size_t x){
+    pos += x;
+    if (pos > SZ)
+        throw 42;
+    return buf + pos - x;
+}
+void operator delete(void *x) {}
