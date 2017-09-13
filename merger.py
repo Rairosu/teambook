238bfa675d60cc907655887d1d197d4670392089
Nikita Mikhaylov <nikitamikhaylov@MacBook-Pro-Nikita.local>
2017-09-13 14:45:38 +0300

    merger added

---
+++ merger.py
@@ -0,0 +1,37 @@
+###
+### This script merges all '.cpp' files into 1
+###
+
+import os
+
+queue = [os.getcwd()]
+que_head = 0
+queue_len = 1
+
+files = []
+
+while que_head < queue_len:
+    cur_location = queue[que_head]
+    que_head += 1
+    listdir = os.listdir(cur_location)
+    for file_name in listdir:
+        cur_file = cur_location + "/" + file_name
+        if os.path.isdir(cur_file):
+            queue.append(cur_file)
+            queue_len += 1
+        else:
+            if '.cpp' in file_name:
+                files.append(cur_file)
+
+result = open("merged.txt", "w")
+
+for i in files:
+    cur_file = open(i, 'r')
+    all_lines = cur_file.readlines()
+    for j in all_lines:
+        result.write(j)
+    result.write('\n\n\n')
+
+print('found %d files:' % len(files))
+for i in files:
+    print (" ", i)
